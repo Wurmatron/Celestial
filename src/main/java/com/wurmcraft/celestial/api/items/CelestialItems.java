cd8bb64cdad687a250b0d58ae934af9d16cbefc6
Wurmatron <wurmatron@gmail.com>
2018-04-02 17:17:07 -0400

    Started Work on Demi, Celestial

--- src/main/java/com/wurmcraft/celestial/api/items/CelestialItems.java
+++ src/main/java/com/wurmcraft/celestial/api/items/CelestialItems.java
@@ -10,4 +10,15 @@ public class CelestialItems {
 	// Special
 	public static Item worldGlobe;
 
+	// Demi
+	public static Item helmDemi;
+	public static Item chestDemi;
+	public static Item legsDemi;
+	public static Item bootsDemi;
+
+	/// Celestial
+	public static Item helmCelestial;
+	public static Item chestCelestial;
+	public static Item legsCelestial;
+	public static Item bootsCelestial;
 }
---
+++ src/main/java/com/wurmcraft/celestial/common/items/demi/DemiArmor.java
@@ -0,0 +1,51 @@
+package com.wurmcraft.celestial.common.items.demi;
+
+import com.wurmcraft.celestial.Celestial;
+import com.wurmcraft.celestial.client.render.ModelDemi;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class DemiArmor extends ItemArmor {
+
+	public DemiArmor (ArmorMaterial material,int renderIndex,EntityEquipmentSlot slot) {
+		super (material,renderIndex,slot);
+		setCreativeTab (Celestial.tabCelestial);
+		setUnlocalizedName ("demi" + slot.name ().substring (0,1) + slot.name ().substring (1,slot.name ().length ()).toLowerCase ());
+	}
+
+	@Nullable
+	@Override
+	public ModelBiped getArmorModel (EntityLivingBase entityLiving,ItemStack itemStack,EntityEquipmentSlot armorSlot,ModelBiped _default) {
+		return new ModelDemi (0.006f,0,64,32);
+	}
+
+	@Override
+	public void addInformation (ItemStack stack,@Nullable World world,List <String> tip,ITooltipFlag adv) {
+		super.addInformation (stack,world,tip,adv);
+	}
+
+	@Override
+	public String getItemStackDisplayName (ItemStack stack) {
+		return "\u00a76" + super.getItemStackDisplayName (stack);
+	}
+
+	@Override
+	public void onArmorTick (World world,EntityPlayer player,ItemStack itemStack) {
+		super.onArmorTick (world,player,itemStack);
+		player.noClip = true;
+		player.capabilities.allowFlying = true;
+		player.capabilities.allowEdit = false;
+		player.capabilities.disableDamage = true;
+		player.capabilities.isFlying = true;
+	}
+}
